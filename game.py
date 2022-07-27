96be68566997acaa05fcc49131e41c030ac3823a
Jiang <jiangyue0380@gmail.com>
2022-07-27 21:04:57 +1000

    updated methods in game.py

---
+++ game.py
@@ -0,0 +1,37 @@
+
+class Game:
+    # the Game class contains all methods for running the Game
+
+    def __init__(self):
+        pass
+
+    def display_game_rule(self):
+        # display the game rule
+        pass
+
+    def human_player_selection(self):
+        # ask how many human players will paly
+        pass
+
+    def ai_player_selection(self):
+        # ask how mamy ai will play
+        pass
+
+    def run_game(self):
+        # game starts
+        # game ends when one play has two wins
+        pass
+
+    def display_winner_for_the_round(self):
+        # display the winner for the round
+        pass
+
+    def display_finnal_winner(self):
+        pass
+
+    def play_again(self):
+        # aks whether user wants to play again
+        pass
+    def game_end(self):
+        # print game ends message
+        pass
