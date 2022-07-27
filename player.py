aa6332fe6853af071c25af58c83f418b0befacbf
Jiang <jiangyue0380@gmail.com>
2022-07-27 21:51:26 +1000

    updated methods in player.py

---
+++ player.py
@@ -0,0 +1,11 @@
+class Player:
+    # Each player has a name
+    # Each player can select gesture,
+    # human players ask to choose the gesture
+    # ai player generate gesture randomly
+
+    def __init__(self):
+        self.player_name = ""
+    
+    def select_gesture(self):
+        pass
