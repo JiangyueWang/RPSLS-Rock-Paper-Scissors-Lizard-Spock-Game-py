d42bfb528f64d14042cc5f29fa89f327f9fa7de5
Jiang <jiangyue0380@gmail.com>
2022-07-30 02:15:19 +1000

    updated rand_index in utility.py, and import into ai.py

--- ai.py
+++ ai.py
@@ -1,5 +1,5 @@
 from player import Player
-import random
+from utility import rand_index
 
 
 class Ai(Player):
@@ -9,12 +9,8 @@ class Ai(Player):
         super().__init__(name)
         self.ai_rand_gesture_index = 0
 
-    def rand_index(self, list_length):
-        self.rand_list_index = random.randint(0, list_length)
-        return self.rand_list_index
-
     def select_gesture(self):
-        self.ai_rand_gesture_index = self.rand_index(len(self.gesture_list)-1)
+        self.ai_rand_gesture_index = rand_index(len(self.gesture_list)-1)
         print(
             f"\n{self.player_name} chose {self.gesture_list[self.ai_rand_gesture_index]}\n")
 
--- utility.py
+++ utility.py
@@ -1,11 +1,7 @@
 import random
 
 
-class Utility:
-    def __init__(self):
-        pass
-
-    def rand_index(self, list_length):
-        # return a random index from a list
-        self.rand_list_index = random.randint(0, list_length)
-        return self.rand_list_index
+def rand_index(list_length):
+    # return a random index from a list
+    rand_list_index = random.randint(0, list_length)
+    return rand_list_index
