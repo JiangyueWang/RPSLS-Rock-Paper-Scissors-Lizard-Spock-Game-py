301e11d9c89ccc1e143b442154f08cad9175b37f
Jiang <jiangyue0380@gmail.com>
2022-07-30 10:34:38 +1000

    updated check_user_input function in utility.py

--- utility.py
+++ utility.py
@@ -8,12 +8,12 @@ def rand_index(list_length):
     return rand_list_index
 
 
-def check_user_input(range):
+def check_user_input(range1):
     while True:
-        user_input = input(f"Enter valid integer from 0 to 4: {range}")
+        user_input = input(f"Enter valid integer from 0 to {range1}:")
         try:
             user_input = int(user_input)
-            if user_input in range(0, 4):
+            if user_input in range(0, range1):
                 break
             else:
                 print("Check your entry again")
@@ -23,6 +23,3 @@ def check_user_input(range):
             continue
 
     return user_input
-
-
-check_user_input()
